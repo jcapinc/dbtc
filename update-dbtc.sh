2bd5ed2a51da4a9a478608338aa5d3f1bc8a57e7
Jeff Parrish <jeffs.linux@gmail.com>
2020-11-02 15:23:24 -0500

    adjusting upgrade script

--- update-dbtc.sh
+++ update-dbtc.sh
@@ -1,14 +1,12 @@
 
 function update_dbtc {
-	cd $1;
+	cd ~/$1;
 	git pull;
 	npm run build;
 	systemctl restart $1
-	cd ..
 }
 
 update_dbtc "dbtc"
-update_dbtc "dbtc1"
 update_dbtc "dbtc2"
 update_dbtc "dbtc3"
 update_dbtc "dbtc4"
